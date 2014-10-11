4baf40460ce4bcd2eef7c10a126007e6d170e76d
Josh Samuelson <js@puppetlabs.com>
2014-10-11 07:58:52 -0700

    Roughed in first working draft

--- conflict-gem.rb
+++ conflict-gem.rb
@@ -1,34 +1,45 @@
-#Get list of packages and versions and put into a 2d array
-#for each value in array
-	#for each result in gem deps -r $name -v $verison
-		#Add to a hash with name as key and version as value
-
-
-#Print out entire hash table including duplicate values
 
 require 'rubygems'
 require 'gems'
 
+$dependencies = Hash.new
 
 #Get all of the dependenciew for a given gem
 def getDeps(name,version)
+	#Returns an array of hashes one for each version
 	deps = Gems.dependencies [name]
+	
+	p "Hitting rubygems.org api for " + name + " version " + version
 
+	#Loop through hash array looking for right version
 	deps.each do |dep|
 		#If it's a > 0 depencency then just use the first one in the array
-		if version == 0 then 
-			version = dep[0][:number]
-		end
-		if dep[:number] == version then
+		if dep[:number] == version or version == 0 then
 			dep[:dependencies].each do |sub_dep|
+				#Recurse on the subdependencies
 				getDeps(sub_dep[0],sub_dep[1].split(' ')[1])
-				p sub_dep
+				#If a value exists append to existing key otherwise make a new one
+				if $dependencies[sub_dep[0]] != nil then
+					$dependencies[sub_dep[0]] = $dependencies[sub_dep[0]] + "," + sub_dep[1]
+				else
+					$dependencies[sub_dep[0]] = sub_dep[1]
+				end
 			end
+		end
+		
+		#Don't print out every possible version
+		if version == 0 then 
+			break 
 		end
 	end
 end
 
-gem_name = 'r10k'
-gem_version = '1.3.4'
+getDeps(ARGV[0],ARGV[1])
 
-getDeps(gem_name,gem_version)
+if $dependencies == {} then
+	p "No dependencies found"
+else
+	$dependencies.each do |key, value|
+		p key + "  " + value
+	end
+end
