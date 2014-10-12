7624ccb0682ec96bc694c8a53a72bdce88c5fa5e
Josh Samuelson <js@puppetlabs.com>
2014-10-11 19:22:38 -0700

    More fiddling, roughed out parser for picking best version

--- conflict-gem.rb
+++ conflict-gem.rb
@@ -1,3 +1,6 @@
+#Query rubygems.org using the "gems" gem
+#find all dependencies and subdependencies for a given gem
+#Requires two arguments package Name and Version, in that order
 
 require 'rubygems'
 require 'gems'
@@ -8,22 +11,20 @@ $dependencies = Hash.new
 def getDeps(name,version)
 	#Returns an array of hashes one for each version
 	deps = Gems.dependencies [name]
-	
-	p "Hitting rubygems.org api for " + name + " version " + version
-
 	#Loop through hash array looking for right version
 	deps.each do |dep|
 		#If it's a > 0 depencency then just use the first one in the array
 		if dep[:number] == version or version == 0 then
 			dep[:dependencies].each do |sub_dep|
-				#Recurse on the subdependencies
-				getDeps(sub_dep[0],sub_dep[1].split(' ')[1])
+
 				#If a value exists append to existing key otherwise make a new one
 				if $dependencies[sub_dep[0]] != nil then
-					$dependencies[sub_dep[0]] = $dependencies[sub_dep[0]] + "," + sub_dep[1]
+					$dependencies[sub_dep[0]] = $dependencies[sub_dep[0]].push(sub_dep[1])
 				else
-					$dependencies[sub_dep[0]] = sub_dep[1]
+					$dependencies[sub_dep[0]] = Array.new [sub_dep[1]]
 				end
+				#Recurse on the subdependencies
+				getDeps(sub_dep[0],sub_dep[1].split(' ')[1])
 			end
 		end
 		
@@ -34,12 +35,27 @@ def getDeps(name,version)
 	end
 end
 
+def parseDeps(deps_list)
+	if deps_list.length == 1 then 
+		return deps_list[0] 
+	end
+	#Sort list by version number
+	#for each
+	#if >= go to next otherwise stop
+	#if <= stop
+	#otherwise return last record
+end
+
 getDeps(ARGV[0],ARGV[1])
 
 if $dependencies == {} then
 	p "No dependencies found"
 else
 	$dependencies.each do |key, value|
-		p key + "  " + value
+		p key
+		value.each do |version|
+			p version
+		end
 	end
 end
+
