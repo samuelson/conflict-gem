9d68403e96bc5f4ebd64b320d2dbca33e827b4e1
Josh Samuelson <js@puppetlabs.com>
2014-10-19 05:14:49 -0700

    Added version picker

--- conflict-gem.rb
+++ conflict-gem.rb
@@ -66,6 +66,71 @@ def getDeps(name,version)
   end
 end
 
+def version_finder(versions)
+  if versions.length == 1 then
+    return versions[0].split(' ')[1]
+  else
+    #Loop through the versions and put into a hash with 3 entries
+    #The smallest 'Less then' the largest 'Greater than' and the largest 'Equal to'
+    results = Hash.new
+    versions.each do |version|
+      #If there is a comma in the version string treat it as separate versions
+      version.split(',').each do |sub_version|
+        value = sub_version.split(' ')[1]
+        case sub_version.split(' ')[0]
+        when '<=','<'
+          if results['L'] == nil then
+            results['L'] = value
+  	  elsif results['L'] >= value then
+            results['L'] = value
+          end
+          puts "L is " + results['L']
+        when '~>','='
+          if results['E'] == nil then
+            results['E'] = value
+          elsif results['E'] <= value then
+            results['E'] = value
+          end
+	  puts "E is " + results['E']
+        when '>=','>'
+          if results['G'] == nil then
+            results['G'] = value
+          elsif results['G'] <= value then
+            results['G'] = value
+          end
+	  puts "G is " + results['G']
+        end
+      end
+    end
+    
+    if results['L'] == nil then
+      if results['G'] == nil then
+        return results['E'] #If only E is present return E
+      elsif results['E'] == nil then
+	return results['G']
+      else #G and E are not nil so compare them
+        if results['E'] >= results['G'] then
+          return results['E']
+        else
+	  return results['G']
+        end
+      end
+    elsif results['G'] != nil then #G and L both exist
+      if results['E'] == nil then
+	return results['L']
+      elsif results['E'] <= results['L'] then
+        return results['E']
+      end
+    else #All three are present
+      if results['E'] <= results['L'] && results['E'] >= results['G'] then
+        return results['E'] #Conflict-free match
+      else
+        return "Conflict: >=" + results['G'] + " ~>" + results['E'] + " <=" + results['L']
+      end
+    end
+  end
+end
+
 ARGV.each do |dep|
 	getDeps(dep.split(',')[0],dep.split(',')[1])
 end
@@ -73,12 +138,17 @@ end
 #Print out in a way that can be easily pasted into the puppet manifest
 $dependencies.each do |key, value|
   #If there is only one version, print it in the final fromat
-  if value.length == 1 then
-    puts "bootstrap::gem { '" + key + ":                     version => '" +  value[0].split(' ')[1] + "' }"
-  else
-    value.each do |version|
-      puts "bootstrap::gem { '" + key + ":                     version => '" +  version + "' }"
-    end
-  end
+#  if value.length == 1 then
+#    puts "bootstrap::gem { '" + key + ":                     version => '" +  value[0].split(' ')[1] + "' }"
+#  else
+#    value.each do |version|
+#      if version.split(' ')[0] == '<=' || '<'
+#      puts "bootstrap::gem { '" + key + ":                     version => '" +  version + "' }"
+#    end
+#  end
+  puts "Finding best match for " + key + " in " + value.to_s
+  puts "Gem: " + key + " version: " + version_finder(value)
 end
+
+
 
